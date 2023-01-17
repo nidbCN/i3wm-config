b2496f6f295a6abdf2002a918acabf235bd14c50
Gaein nidb <mail@gaein.cn>
2023-01-17 22:41:09 +0800

    add config and statusbar config

---
+++ link.sh
@@ -0,0 +1,5 @@
+rm ~/.config/i3/config
+ln -s ~/Documents/Config/i3wm-config/i3/config ~/.config/i3/config
+
+mkdir ~/.config/i3status
+ln -s ~/Documents/Config/i3wm-config/i3status/config ~/.config/i3status/config
