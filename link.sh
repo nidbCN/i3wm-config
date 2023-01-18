6f6b1d46354c28e12c30cf8d8d09dacd45bdc638
Gaein nidb <mail@gaein.cn>
2023-01-19 01:28:42 +0800

    feat:add top bar

--- link.sh
+++ link.sh
@@ -3,3 +3,4 @@ ln -s ~/Documents/Config/i3wm-config/i3/config ~/.config/i3/config
 
 mkdir ~/.config/i3status
 ln -s ~/Documents/Config/i3wm-config/i3status/config ~/.config/i3status/config
+ln -s ~/Documents/Config/i3wm-config/i3status/config_top ~/.config/i3status/config_top
