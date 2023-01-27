b8b382b33ee424fb47ea072629bbbdac40a64c49
Gaein nidb <mail@gaein.cn>
2023-01-27 22:22:39 +0800

    feat: ad i3lock-color config

--- link.sh
+++ link.sh
@@ -1,6 +1,7 @@
 rm ~/.config/i3/config
-ln -s ~/Documents/Config/i3wm-config/i3/config ~/.config/i3/config
+ln -s $(pwd)/i3/config ~/.config/i3/config
 
-mkdir ~/.config/i3status
-ln -s ~/Documents/Config/i3wm-config/i3status/config ~/.config/i3status/config
-ln -s ~/Documents/Config/i3wm-config/i3status/config_top ~/.config/i3status/config_top
+mkdir -p ~/.config/i3status
+ln -s $(pwd)/i3status/config ~/.config/i3status/config
+ln -s $(pwd)/i3status/config_top ~/.config/i3status/config_top
+ln -s $(pwd)/lock ~/lock
